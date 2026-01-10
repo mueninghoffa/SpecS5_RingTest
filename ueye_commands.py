b2ecb67a941e511040bedb447bf8b50d71278cb3
Aaron Mueninghoff <mueninghoffa@gmail.com>
2026-01-09 17:17:40 -0800

    Added predicates back into UeyeCommand, and defined them for gain and gamma.

--- ueye_commands.py
+++ ueye_commands.py
@@ -4,13 +4,15 @@ Wrapper for pyueye functions. Written in part by ChatGPT.
 
 from __future__ import annotations
 
-from typing import Any, Callable, Optional
+from typing import Any, Callable, Optional, cast
 
 from pyueye import ueye
 
 from logging_config import get_logger
 
 logger = get_logger(__name__)
+
+Predicate = Callable[[int, tuple[int, ...]], bool]
 
 
 class PyUeyeError(RuntimeError):
@@ -50,24 +52,23 @@ class UeyeCommand:
     func : `ueye` function
         The wrapped function.
     name : str
-        Name of the function. Defaults to `self.func.__name__` if not
+        Name of the function. Defaults to ``self.func.__name__`` if not
         initialized with a `name` supplied.
-        return value from `func` and false otherwise.
 
     Raises
     ------
     `PyUeyeError`
-        Raised if the return value is not `ueye.IS_SUCCESS`.
+        Raised if the return value is not ``ueye.IS_SUCCESS`.`
 
     Notes
     -----
     This is to simplify and reduce the verbosity of calling pyueye
     functions and verifying their success. Commonly used functions should
-    be wrapped in this class. If a return value other than
-    `ueye.IS_SUCCESS` would be acceptable, it should be handled explicitly.
-    For example, one might handle `ueye.is_FreezeVideo` returning
-    `ueye.IS_TIMED_OUT` more gracefully than the raising of a
-    `PyUeyeError`.
+    be wrapped in this class. Most `ueye` functions return only a status
+    code, such as ``ueye.IS_SUCCESS``, but a few functions may also return
+    a value, such as ``ueye.is_SetHardwareGain()`` when the value of arg
+    ``nMaster`` is one of ``ueye.IS_GET_...``. Supplying a `success`
+    predicate allows for the handling of these functions.
     """
 
     def __init__(
@@ -75,17 +76,46 @@ class UeyeCommand:
         func: Callable[..., Any],
         *,
         name: Optional[str] = None,
+        accept: Optional[Predicate] = None,
     ):
         self.func = func
         self.name = name or func.__name__
+        if accept is None:
+            accept = lambda _, __: False  # noqa: E731
+        self.accept = cast(Predicate, accept)
 
-    def __call__(self, *args, camera_handle: Optional[ueye.HIDS] = None) -> bool:
+    def __call__(self, *args, camera_handle: Optional[ueye.HIDS] = None) -> bool | int:
+        """
+        Evaluates the success of calling the wrapped function.
+
+        Parameters
+        ----------
+        *args
+            Arguments to be passed to `self.func`.
+        camera_handle : `ueye.HIDS`, optional
+            Camera handle for gettings errors if they occur. If the first
+            arg is a camera handle, it does not need to be supplied
+            separately.
+
+        Returns
+        -------
+        result : bool, int
+            Returns the result of the function call, if it succeeded.
+
+        Raises
+        ------
+        PyUeyeError
+            Raised if the function call did not succeed (`self.accept`
+            returned ``False``).
+        """
         result = self.func(*args)
 
-        if result == ueye.IS_SUCCESS:
-            return True
+        if result == ueye.IS_SUCCESS or self.accept(result, args):
+            return result
 
         error_text = None
+        if camera_handle is None and isinstance(args[0], ueye.c_uint):
+            camera_handle = args[0]
         if camera_handle is not None:
             error_code = ueye.int()
             error_message = ueye.char_p()
@@ -99,7 +129,34 @@ class UeyeCommand:
         raise PyUeyeError(self.name, int(result), error_text)
 
 
-# ---- Common commands used by the camera and driver ----
+# ---- Predicates ----
+
+
+def gain_accept(result: int, args: tuple[int, ...]) -> bool:
+    getters = {
+        ueye.IS_GET_MASTER_GAIN,
+        ueye.IS_GET_RED_GAIN,
+        ueye.IS_GET_GREEN_GAIN,
+        ueye.IS_GET_BLUE_GAIN,
+        ueye.IS_GET_DEFAULT_MASTER,
+        ueye.IS_GET_DEFAULT_RED,
+        ueye.IS_GET_DEFAULT_GREEN,
+        ueye.IS_GET_DEFAULT_BLUE,
+    }
+    if args[1] in getters:
+        return 0 <= result <= 100
+    return False
+
+
+def gamma_accept(result: int, args: tuple[int, ...]) -> bool:
+    if args[1] == ueye.IS_GET_HW_GAMMA:
+        return isinstance(result, int) and result >= 0  # I think this is correct?
+    if args[1] == ueye.IS_GET_HW_SUPPORTED_GAMMA:
+        return result in (ueye.IS_SET_HW_GAMMA_ON, ueye.IS_SET_HW_GAMMA_OFF)
+    return False
+
+
+# ---- Common commands that only return status ----
 
 init_camera = UeyeCommand(ueye.is_InitCamera)
 exit_camera = UeyeCommand(ueye.is_ExitCamera)
@@ -113,12 +170,10 @@ set_image_mem = UeyeCommand(ueye.is_SetImageMem)
 
 get_camera_info = UeyeCommand(ueye.is_GetCameraInfo)
 get_sensor_info = UeyeCommand(ueye.is_GetSensorInfo)
-get_device_info = UeyeCommand(ueye.is_DeviceInfo)
+device_info = UeyeCommand(ueye.is_DeviceInfo)
 
 exposure_cmd = UeyeCommand(ueye.is_Exposure)
-gamma_cmd = UeyeCommand(ueye.is_Gamma)
 pixel_clock_cmd = UeyeCommand(ueye.is_PixelClock)
-set_hardware_gain = UeyeCommand(ueye.is_SetHardwareGain)
 aoi_cmd = UeyeCommand(ueye.is_AOI)
 set_binning = UeyeCommand(ueye.is_SetBinning)
 
@@ -127,5 +182,11 @@ get_camera_list = UeyeCommand(ueye.is_GetCameraList)
 set_camera_id = UeyeCommand(ueye.is_SetCameraID)
 # Using set_camera_id to get the current camera ID will result in a PyUeyeError
 #   use get_device_info or get_camera_list instead
+
+# ---- Common commands that might return a value ----
+
+set_hardware_gain = UeyeCommand(ueye.is_SetHardwareGain, accept=gain_accept)
+set_hardware_gamma = UeyeCommand(ueye.is_SetHardwareGamma, accept=gamma_accept)
+
 
 logger.info("ueye command wrappers instantiated")
